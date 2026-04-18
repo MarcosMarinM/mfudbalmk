55c1d6bc0b14d684f001c06e333771e94a76befb
Marcos Marín Martín <marcosstilton@gmail.com>
2026-04-18 23:25:22 +0200

    Remove scratch folder from repository and update .gitignore

--- scratch/diagnose_equipo.R
+++
@@ -1,9 +0,0 @@
-load("entorno_procesado.RData")
-library(dplyr)
-library(stringr)
-
-test_df <- career_summary_jugadoras_df %>%
-  filter(str_detect(equipo, "Rabotn|Работнички")) %>%
-  select(id, competicion_nombre, equipo) %>%
-  head(5)
-print(test_df)
--- scratch/diagnose_html.R
+++
@@ -1,6 +0,0 @@
-source("R/07_setup.R")
-source("R/08_functions.R")
-load("entorno_procesado.RData")
-
-span_result <- entity_name_spans("\u0420\u0430\u0431\u043e\u0442\u043d\u0438\u0447\u043a\u0438")
-print(as.character(span_result))
